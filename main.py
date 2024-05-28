562b09100e9878af13290a67b9508e41c1003f43
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-05-27 21:56:52 -0400

    Started a comprehensive Python Course

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# This gets the first and last name from the user
+first_name = "Jordan"
+last_name = "Haynes"
+
+# This combines the two variables together
+full_name = f"{first_name} {last_name}"
+
+# This prints the message to the terminal
+print(f"Hello, {full_name}!")
+# ---------------------------------------------------
+
+# Integers
+age = 23
+print(f"It seems you are {age} years old!")
+
+# Float
+height = 250.5
+print(f"My data says that you are {height}cm tall!")
+
+# Booleans
+human = False
+print(f"Data says your status of human is: {human}")
