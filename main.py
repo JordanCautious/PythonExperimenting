572222ed50d0731d053fcb8d0271ca7cee5d9f40
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-05-28 00:08:56 -0400

    A basic program

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-
 # This gets the first and last name from the user
-first_name = "Jordan"
-last_name = "Haynes"
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+age = int(input("What is your last age?: "))
+height = float(input("What is your last height?: "))
+human = input("Are you human?(Y/N): ")
 
 # This combines the two variables together
 full_name = f"{first_name} {last_name}"
 
+# Converting the response to boolean
+if human == "Y":
+    human = True
+elif human == "N":
+    human = False
+else:
+    print("This should not be possible!")
+
+# Converting boolean to appropriate phrase
+if not human:
+    outcome = "are not"
+else:
+    outcome = "are"
+
 # This prints the message to the terminal
-print(f"Hello, {full_name}!")
-# ---------------------------------------------------
-
-# Integers
-age = 23
-print(f"It seems you are {age} years old!")
-
-# Float
-height = 250.5
-print(f"My data says that you are {height}cm tall!")
-
-# Booleans
-human = False
-print(f"Data says your status of human is: {human}")
+print(f"Hello, {full_name}! You are {age} years old with a height of {height}cm! You {outcome} human!")
