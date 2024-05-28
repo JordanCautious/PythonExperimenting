37d97cb03965e400715d41eeed049e60e79ccfa9
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-05-28 15:10:05 -0400

    String slicing

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-# This gets the first and last name from the user
-first_name = input("What is your first name?: ")
-last_name = input("What is your last name?: ")
-age = int(input("What is your last age?: "))
-height = float(input("What is your last height?: "))
-human = input("Are you human?(Y/N): ")
+# This is a testing ground for Python code
 
-# This combines the two variables together
-full_name = f"{first_name} {last_name}"
+# String Slicing
+name = "Jordan Haynes"
 
-# Converting the response to boolean
-if human == "Y":
-    human = True
-elif human == "N":
-    human = False
-else:
-    print("This should not be possible!")
+first_name = name[:7]
+last_name = name[7:]
+reversed_name = name[::-1]
 
-# Converting boolean to appropriate phrase
-if not human:
-    outcome = "are not"
-else:
-    outcome = "are"
+full_name = first_name + last_name
 
-# This prints the message to the terminal
-print(f"Hello, {full_name}! You are {age} years old with a height of {height}cm! You {outcome} human!")
+print(full_name)
+print(reversed_name)
