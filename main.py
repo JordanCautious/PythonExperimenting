7382967861afe7a856998d0912be3f0e24b66995
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-05-30 14:53:24 -0400

    Practice with Modules

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 # This is a testing ground for Python code
 
-# Practice with Exceptions
+# Practice with Modules
+import messages as msg
 
-try:
-    numerator = int(input("Enter a number to divide: "))
-    denominator = int(input("Enter a number to divide by: "))
-    result = numerator / denominator
-except ZeroDivisionError as e:
-    print(e)
-    print("You can't divide by zero!")
-except ValueError as e:
-    print(e)
-    print("You can only use numbers!")
-except Exception as e:
-    print(e)
-    print("Something went wrong!")
-else:
-    print(f"Your result is {result}. No exceptions have been found!")
-finally:
-    print("This code will always run")
+msg.hello()
+msg.bye()
