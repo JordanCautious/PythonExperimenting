c382441f450428ec7a8b8546753b4f83b9f80d1c
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-05-30 15:15:22 -0400

    Practice with making Rock, Paper, Scissors in Python

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 # This is a testing ground for Python code
 
-# Practice with Modules
-import messages as msg
+# Practice with Rock, Paper, Scissors in Python
+import random
 
-msg.hello()
-msg.bye()
+while True:
+    choices = ["Rock", "Paper", "Scissors"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("Rock, Paper, or Scissors?: ").capitalize()
+
+    print(f"The computer chose {computer}!")
+    print(f"The player chose {player}!")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("Computer wins!")
+        elif computer == "Scissors":
+            print("Player wins!")
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("Computer wins!")
+        elif computer == "Rock":
+            print("Player wins!")
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("Computer wins!")
+        elif computer == "Paper":
+            print("Player wins!")
+
+    play_again = input("Play again? (Y/N): ").capitalize()
+
+    if play_again != "Y":
+        break
+
+print("Bye!")
--- messages.py
+++
@@ -1,6 +0,0 @@
-def hello():
-    print("Hello! Have a nice day!")
-
-
-def bye():
-    print("Goodbye! See you later!")
